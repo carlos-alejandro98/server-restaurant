aa75a7f22d8f789be3766f4a75d33796ac0c5529
Carlos Abarca <45491971+carlos-alejandro98@users.noreply.github.com>
2022-07-03 12:37:30 -0400

    Crud Productos

---
+++ models/producto.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      text: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    tipoProducto: {
+      type: ObjectId,
+      ref: "TipoProducto",
+    },
+    imagen: {
+        type: String,
+        trim: true,
+        required: true,
+        text: true,
+    },
+    status: {
+      type: String,
+      default: "Active",
+      enum: ["Active", "Inactive"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Producto", productSchema);
---
+++ routes/producto.js
@@ -0,0 +1,166 @@
+const express = require("express");
+const router = express.Router();
+
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controller middlewares
+const { obtenerProductos, crearProducto, cantidadProductos, actualizarProducto, cambiarEstado, eliminarProducto } = require("../controllers/producto");
+
+// routes-endpoints
+
+// ROUTES - SWAGGER
+
+/**
+ * @swagger
+ * /productos/obtener-productos:
+ *   get:
+ *     tags:
+ *       - name: "Productos"
+ *     summary: "Obtener todos los productos"
+ *     responses:
+ *       200: 
+ *          description: ok   
+ */
+ router.get("/productos/obtener-productos", obtenerProductos);
+
+/**
+ * @swagger
+ * /productos/create-producto:
+ *   post:
+ *     summary: Crear Producto
+ *     tags:
+ *       - name: "Productos"
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Producto"
+ *     responses:
+ *       200: 
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Producto"
+ *       400:
+ *         description: bad request     
+ */
+router.post("/productos/create-producto", crearProducto);
+
+/**
+ * @swagger
+ * /productos/producto-delete/{nombre}:
+ *   patch:
+ *     tags:
+ *       - name: "Productos"
+ *     summary: "Actualizar Estado Producto"
+ *     parameters:
+ *       - name: "nombre"
+ *         in: "path"
+ *         description: "nombre search"
+ *         required: true
+ *         type: "string"
+ *         trim: true
+ *         text: true
+ *     responses:
+ *       200: 
+ *          description: ok   
+ */
+router.patch("/productos/producto-delete/:slug", cambiarEstado); // soft-delete
+
+/**
+ * @swagger
+ * /productos/remove-producto/{nombre}:
+ *   delete:
+ *     tags:
+ *       - name: "Productos"
+ *     summary: "Eliminar Producto"
+ *     parameters:
+ *       - name: "nombre"
+ *         in: "path"
+ *         description: "nombre search"
+ *         required: true
+ *         type: "string"
+ *         trim: true
+ *         text: true
+ *     responses:
+ *       200: 
+ *          description: ok   
+ */
+ router.delete("/productos/remove-producto/:slug", eliminarProducto); 
+
+
+/**
+ * @swagger
+ * /productos/producto-update/{nombre}:
+ *   put:
+ *     summary: Actualizar Producto
+ *     tags:
+ *       - name: "Productos"
+ *     parameters:
+ *       - name: "nombre"
+ *         in: "path"
+ *         description: "nombre search"
+ *         required: true
+ *         type: "string"
+ *         trim: true
+ *         text: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Producto"
+ *     responses:
+ *       200: 
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Producto"
+ *       400:
+ *         description: bad request     
+ */
+router.put("/productos/producto-update/:slug", actualizarProducto);
+
+
+module.exports = router;
+
+// SCHEMAS - SWAGGER
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Producto:
+ *       type: object
+ *       required:
+ *         - nombre
+ *       properties:
+ *         nombre:
+ *            type: string
+ *            trim: true
+ *            maxlength: 32
+ *         slug:
+ *            type: string
+ *            unique: true
+ *            lowercase: true
+ *            index: true
+ *         status:
+ *            type: string
+ *            default: "Active"
+ *            enum:
+ *            - "Active"
+ *            - "Inactive"
+ *          imagen:
+ *            type: string
+ *            trim: true
+ *       example:
+ *         nombre: Manzana
+ *         slug: frutas-verduras
+ *         tipoProducto: FB
+ *         imagen: /ruta
+ *         status: Active      
+ */
