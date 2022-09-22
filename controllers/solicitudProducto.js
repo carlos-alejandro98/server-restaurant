133721ef89d5b378926747ee61affe339c83bcd4
Alexander Ortiz <CL198785156@enel.com>
2022-09-22 00:26:01 -0300

    Update request product

--- controllers/solicitudProducto.js
+++ controllers/solicitudProducto.js
@@ -7,57 +7,40 @@ const STATUS_CODES = {
   INVALID: "INVALID",
 };
 
-// Crear Solicitudes Old
-/* exports.solicitarProductoold = async (req, res) => {
-  try {
-    const { product, count } = req.body
-    const products = await Producto.find({ name: product })
-    if (products.length > 0) {
-      let firstProduct = products[0]
-      
-    } else {
-      res.status(200).json({
-        CodeResult: STATUS_CODES.INVALID,
-        errorMessage: "No se encontraron productos"
-      })
-    }
-  } catch (err) {
-    res.status(200).json({
-      errorMessage: err.message,
-      CodeResult: STATUS_CODES.ERROR,
-    });
-  }
-}; */
-
 // Crear solicitud
 exports.solicitarProducto = async (req, res) => {
   try {
-    const { producto } = req.body
-    producto.map(async prod => {
-      const products = await Producto.find({ name: prod.product })
+    const { producto } = req.body;
+    const isProduct = producto.map(async (prod) => {
+      const products = await Producto.find({ name: prod.product });
       if (products.length > 0) {
-        let firstProduct = products[0]
+        let firstProduct = products[0];
         if (firstProduct.countStock > 0) {
-          firstProduct.countStock = firstProduct.countStock - prod.count
+          firstProduct.countStock = firstProduct.countStock - prod.count;
           await new Producto(firstProduct).save();
-          const newSolicitudProducto = await new Solicitud(req.body).save();
-          res.status(200).json({
-            CodeResult: STATUS_CODES.SUCCESS,
-            solicitudProduct: newSolicitudProducto
-          })
+          return true;
         } else {
           res.status(200).json({
             CodeResult: STATUS_CODES.INVALID,
-            errorMessage: `No existe Stock disponible para el producto ${product}`
-          })
+            errorMessage: `No existe Stock disponible para el producto ${product}`,
+          });
+          return false;
         }
       } else {
+        res.status(200).json({
+          CodeResult: STATUS_CODES.INVALID,
+          errorMessage: "No se encontraron productos",
+        });
+        return false;
+      }
+    });
+    if (isProduct) {
+      const newSolicitudProducto = await new Solicitud(req.body).save();
       res.status(200).json({
-        CodeResult: STATUS_CODES.INVALID,
-        errorMessage: "No se encontraron productos"
-      })
+        CodeResult: STATUS_CODES.SUCCESS,
+        solicitudProduct: newSolicitudProducto,
+      });
     }
-    })
   } catch (err) {
     res.status(200).json({
       errorMessage: err.message,
@@ -66,18 +49,14 @@ exports.solicitarProducto = async (req, res) => {
   }
 };
 
-
 // Obtener todos los tipos de productos
 exports.obtenerSolicitudes = async (req, res) => {
-  let solicitudes = await Solicitud.find()
-      .exec();
+  let solicitudes = await Solicitud.find().exec();
   res.status(200).json({
     CodeResult: STATUS_CODES.SUCCESS,
-    solicitudes
+    solicitudes,
   });
 };
-
-
 
 //cambiar estados
 /* exports.cambiarEstado = async (req, res) => {
@@ -101,55 +80,52 @@ exports.obtenerSolicitudes = async (req, res) => {
   }
 }; */
 
-
 exports.cambiarEstado = async (req, res) => {
   const estado = req.body.status;
   try {
     const updated = await Solicitud.findOneAndUpdate(
       { _id: req.params.producto },
-      { status: estado === "Solicitado" ?  "Pendiente" : "Entregado"},
+      { status: estado === "Solicitado" ? "Pendiente" : "Entregado" },
       { new: true }
     ).exec();
     res.status(200).json({
       CodeResult: STATUS_CODES.SUCCESS,
-      updated
+      updated,
     });
   } catch (err) {
     console.log("Error al actualizar el producto: ", err);
     // return res.status(400).send("Product update failed");
     res.status(400).json({
       errorMessage: "Error al actualizar el producto",
-      CodeResult: STATUS_CODES.ERROR
+      CodeResult: STATUS_CODES.ERROR,
     });
   }
 };
 
-
 exports.deleteRequestProduct = async (req, res) => {
-try {
-  const { id, count, name } = req.body
-  const request = await Solicitud.find({ _id: id })
-  if (request.length > 0) {
-    const products = await Producto.find({ name })
-    let firstProduct = products[0]
-    firstProduct.countStock = firstProduct.countStock + count
-    await new Producto(firstProduct).save();
-    await Solicitud.deleteOne({ _id: id })
-    res.status(200).json({
-      CodeResult: STATUS_CODES.SUCCESS,
-      message: "Solicitud eliminada correctamente"
-    })
-  } else {
-    res.status(200).json({
-      errorMessage: "Hubo un error al eliminar solicitud de producto",
-      CodeResult: STATUS_CODES.INVALID,
+  try {
+    const { id, count, name } = req.body;
+    const request = await Solicitud.find({ _id: id });
+    if (request.length > 0) {
+      const products = await Producto.find({ name });
+      let firstProduct = products[0];
+      firstProduct.countStock = firstProduct.countStock + count;
+      await new Producto(firstProduct).save();
+      await Solicitud.deleteOne({ _id: id });
+      res.status(200).json({
+        CodeResult: STATUS_CODES.SUCCESS,
+        message: "Solicitud eliminada correctamente",
+      });
+    } else {
+      res.status(200).json({
+        errorMessage: "Hubo un error al eliminar solicitud de producto",
+        CodeResult: STATUS_CODES.INVALID,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      errorMessage: error.message,
+      CodeResult: STATUS_CODES.ERROR,
     });
   }
-} catch (error) {
-  res.status(500).json({
-    errorMessage: error.message,
-    CodeResult: STATUS_CODES.ERROR,
-  });
-}
-}
-
+};
